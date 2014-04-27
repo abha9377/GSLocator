94b6500a96a120686c45afe0b255dae6d9352ace
abha9377 <abha9377@gmail.com>
2014-04-27 14:57:51 +0100

    final draft

---
+++ public/events.js
@@ -0,0 +1,183 @@
+([
+                        {
+                          "start" : "2013-11-12T19:00:00+00",
+                          "end" : "2013-11-12T23:00:00+00",
+                          "point" : {
+                              "lat" : 50.933984,
+                              "lon" : -1.397861
+                           },
+                          "title" : "Greek Society Meeting",
+                          "options" : {
+                            // set the full HTML for the info window
+                            "infoHtml": "<div class='custominfostyle'><b>Greek Society</b> are socialising at the Bar with QUiz and Curry.</div>"
+                          }
+                        },
+                        {
+                          "start" : "2013-11-12T16:00:00+00",
+                          "end" : "2013-11-12T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.934288,
+                              "lon" : -1.397738
+                           },
+                          "title" : "Krishna Society prayer meeting",
+                          "options" : {
+                            "infoHtml": "<div class='custominfostyle'><b>Krishna Society</b> are meditating at the Cube</div>",
+                            "theme": "blue"
+                          }
+                        },
+                        {
+                          "start" : "2013-11-12T18:00:00+00",
+                          "end" : "2013-11-12T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.934176,
+                              "lon" : -1.397662
+                           },
+                          "title" : "Day of Disney Movie Night",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Union Films</b> present the Day of Disney at the Box Office</div>",
+                            "theme": "yellow"
+                          }
+                        },
+												{
+                          "start" : "2013-11-13T14:00:00+00",
+                          "end" : "2013-11-13T20:00:00+00",
+                          "point" : {
+                              "lat" : 50.934203,
+                              "lon" : -1.397512
+                           },
+                          "title" : "General Society Meet",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Asian Society</b> Annual Meet at Building 42</div>",
+                            "theme": "blue"
+                          }
+                        },
+												{
+                          "start" : "2013-11-13T20:00:00+00",
+                          "end" : "2013-11-13T23:30:00+00",
+                          "point" : {
+                              "lat" : 50.934176,
+                              "lon" : -1.397662
+                           },
+                          "title" : "Harry Potter and the Deathly Hallows Part 1",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Union Films</b> present the Harry Potter at the Box Office</div>",
+                            "theme": "yellow"
+                          }
+                        },
+												{
+                          "start" : "2013-11-14T16:00:00+00",
+                          "end" : "2013-11-14T20:00:00+00",
+                          "point" : {
+                              "lat" : 50.93399,
+                              "lon" : -1.396761
+                           },
+                          "title" : "Swimming meet",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Swimming Society</b> are meeting at Jubilee Sports Hall</div>",
+                            "theme": "green"
+                          }
+                        },
+												{
+                          "start" : "2013-11-12T18:00:00+00",
+                          "end" : "2013-11-12T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.933984,
+                              "lon" : -1.397861
+                           },
+                          "title" : "Quiz and Curry",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Philipino Society </b> are hosting a quiz and curry night at the Bridge Bar</div>",
+                            "theme": "blue"
+                          }
+                        },
+												{
+                          "start" : "2013-11-13T18:00:00+00",
+                          "end" : "2013-11-13T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.934176,
+                              "lon" : -1.397662
+                           },
+                          "title" : "Armageddon",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Union Films</b> present the Armageddon at the Box Office</div>",
+                            "theme": "yellow"
+                          }
+                        },
+												{
+                          "start" : "2013-11-13T18:00:00+00",
+                          "end" : "2013-11-13T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.935941,
+                              "lon" : -1.398671
+                           },
+                          "title" : "Diwali Festival: Lighting Diyas",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Asian Society</b> is celebrating Diwali at Turner Sims</div>",
+                            "theme": "blue"
+                          }
+                        },
+												{
+                          "start" : "2013-11-17T16:00:00+00",
+                          "end" : "2013-11-117T20:00:00+00",
+                          "point" : {
+                              "lat" : 50.93399,
+                              "lon" : -1.396761
+                           },
+                          "title" : "Swimming meet",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Swimming Society</b> are meeting at Jubilee Sports Hall</div>",
+                            "theme": "green"
+                          }
+                        },
+												{
+                          "start" : "2013-11-15T16:00:00+00",
+                          "end" : "2013-11-15T20:00:00+00",
+                          "point" : {
+                              "lat" : 50.93399,
+                              "lon" : -1.396761
+                           },
+                          "title" : "Swimming meet",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Swimming Society</b> are meeting at Jubilee Sports Hall</div>",
+                            "theme": "green"
+                          }
+                        },
+												{
+                          "start" : "2013-11-16T16:00:00+00",
+                          "end" : "2013-11-16T20:00:00+00",
+                          "point" : {
+                              "lat" : 50.93399,
+                              "lon" : -1.396761
+                           },
+                          "title" : "Swimming meet",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Swimming Society</b> are meeting at Jubilee Sports Hall</div>",
+                            "theme": "green"
+                          }
+                        },
+												{
+                          "start" : "2013-11-16T18:00:00+00",
+                          "end" : "2013-11-16T22:00:00+00",
+                          "point" : {
+                              "lat" : 50.934176,
+                              "lon" : -1.397662
+                           },
+                          "title" : "The Social Network",
+                          "options" : {
+                            // use the default title/description info window
+                            "infoHtml": "<div class='custominfostyle'><b>Union Films</b> present the Social Network at the Box Office</div>",
+                            "theme": "yellow"
+                          }
+                        }
+                    ]
+)
--- public/prog.php
+++ public/prog.php
@@ -1,21 +1,57 @@
-<?
+<?php
+
 // get timestamps
 $start = strtotime($_GET['start']);
 $end = strtotime($_GET['end']);
 $cb = $_GET['callback'];
- 
+
+$category = array("Cultural", "Sports", "Rest");
+
+$society = array(
+		array("Greek", "Asian", "Chinese", "Libyan"),
+		array("Swimming", "Horse-riding", "Fencing", "Squash"),
+		array("Krishna", "Latino-dancing", "Ballroom-dancing", "Shakespearean")
+		);
+		
+$type = array("Meeting", "Get-together", "Social");
+
+$point = array(
+	array("The Bridge Bar", 50.933984, -1.397861),
+	array("The Cube", 50.934288, -1.397738),
+	array("Building 42", 50.934203, -1.397512),
+	array("Turner Sims", 50.935941, -1.398671),
+	array("Jubilee Sports Hall", 50.93399, -1.396761)
+	);
+	
 // start data
 echo "$cb([";
  
-// make some fake data
-for ($x=0; $x<20; $x++) {
+// random data simulation data
+for ($x=0; $x<100; $x++) {
+  
   if ($x) echo ',';
-  echo '{';
-  echo 'title:"Item '.$x.'",';
-  echo 'start:"'.date('Y-m-d\TH:i:sO', rand($start, $end)).'",';
-  echo 'point:{lat:'.(50.9 + 10*(float)rand()/(float)getrandmax())
-       .',lon:'.(-1.3 + 10*(float)rand()/(float)getrandmax()).'}';
-  echo '}';
+  
+  $index = mt_rand(0,2);
+  $soc = mt_rand(0,3);
+  $bldg = mt_rand(0,4);
+  $t = mt_rand(0,2);
+  
+  $time = rand ($start, $end);
+  $interval = mt_rand(1, 3)*60*60 ;
+  
+	 echo '{';
+	 echo 'start:"' . date('Y-m-d\TH:i:sO', $time) . '",'; 
+	 echo 'end:"' . date('Y-m-d\TH:i:sO', $time+$interval) . '",'; 
+	 echo 'point:{lat:'. $point[$bldg][1].',lon:'.$point[$bldg][2].'},';
+	 echo 'title:" '.$society[$index][$soc].' Society '. $type[$t] . '",';
+	 echo 'options:{infoHtml:"'. $society[$index][$soc] . ' Society have a ' . $type[$t] . ' <br />&nbsp;at <b>' . $point[$bldg][0]. '</b><br />&nbsp; on <b>' .date('l, d-M-y', $time). '</b><br />&nbsp; from <b>'. date('H:i A', $time) .'</b><br />&nbsp; till <b>'. date('H:i A', $time+$interval).'</b>",';
+	 echo 'tags: ["'. $category[$index] . '","' . $society[$index][$soc] . '","'. $point[$bldg][0] . '","' . date('l', $time) . '","' . date('Y-m-d', $time).'"],';
+	 if($index == 0) echo 'theme:"blue"}';
+	 else if($index == 1) echo 'theme:"green"}';
+	 else echo 'theme:"red"}';
+	 
+	 echo '}';
+ 
 }
  
 // end data
